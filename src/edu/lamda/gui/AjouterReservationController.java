8222d39e16de857f592de41fea1a78e1ba86b657
SofieneLetaief <sofiene.letaief1@esprit.tn>
2021-03-25 23:35:14 +0100

    update UI

--- src/edu/lamda/gui/AjouterReservationController.java
+++ src/edu/lamda/gui/AjouterReservationController.java
@@ -8,15 +8,20 @@ package edu.lamda.gui;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -31,35 +36,43 @@ public class AjouterReservationController implements Initializable {
     @FXML
     private JFXButton cancelbtn;
     @FXML
-    private Label ticketPriceLabel;
-    @FXML
     private Label errorlabel;
-    @FXML
-    private JFXCheckBox checkbox;
-    @FXML
-    private JFXButton sharebtn;
-    @FXML
-    private JFXTextField titlefield;
-    @FXML
-    private JFXTextArea subjectfield;
-    @FXML
-    private Label datelabel;
-    @FXML
-    private Label timelabel;
-    @FXML
-    private JFXComboBox<?> groupeCombo;
-    @FXML
-    private JFXButton loeadbtn;
-    @FXML
-    private JFXTextField imagefield;
-    @FXML
-    private ImageView selectedimage;
     @FXML
     private AnchorPane toppane;
     @FXML
     private JFXButton minimisebtn;
     @FXML
     private JFXButton closebtn;
+    @FXML
+    private TableView<?> TableV;
+    @FXML
+    private TableColumn<?, ?> tvId;
+    @FXML
+    private TableColumn<?, ?> tvNom;
+    @FXML
+    private TableColumn<?, ?> tvLieu;
+    @FXML
+    private TableColumn<?, ?> tvHeure;
+    @FXML
+    private TableColumn<?, ?> tvNbpart;
+    @FXML
+    private TableColumn<?, ?> tvDate;
+    @FXML
+    private FontAwesomeIconView bnRefresh;
+    @FXML
+    private JFXButton AddEvent;
+    @FXML
+    private JFXButton updateEvent;
+    @FXML
+    private JFXTextField nomTf;
+    @FXML
+    private JFXTextField lieuTf;
+    @FXML
+    private JFXDatePicker datePicker;
+    @FXML
+    private JFXTextField heureTf;
+    @FXML
+    private JFXTextField nombreTf;
 
     /**
      * Initializes the controller class.
@@ -69,17 +82,8 @@ public class AjouterReservationController implements Initializable {
         // TODO
     }    
 
-    @FXML
-    private void AjouterPoste(ActionEvent event) {
-    }
 
-    @FXML
-    private void selectGroupe(ActionEvent event) {
-    }
 
-    @FXML
-    private void handleButtons(ActionEvent event) {
-    }
 
     
     
@@ -96,6 +100,18 @@ public class AjouterReservationController implements Initializable {
 
     @FXML
     private void handleClose(ActionEvent event) {
+    }
+
+    @FXML
+    private void refreshTable(MouseEvent event) {
+    }
+
+    @FXML
+    private void ajouteEvent(ActionEvent event) {
+    }
+
+    @FXML
+    private void updateEvent(ActionEvent event) {
     }
     
 }
--- src/edu/lamda/gui/Main.java
+++ src/edu/lamda/gui/Main.java
@@ -29,7 +29,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
      
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("Acceuil.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("GestionEvenement.fxml"));
             Scene scene = new Scene(root);
             
             primaryStage.setTitle("");
