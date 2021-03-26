720dc5ebdc329722ca0f294132b828c8618cebe9
SofieneLetaief <sofiene.letaief1@esprit.tn>
2021-03-26 20:06:46 +0100

    add Gestion Reservation

---
+++ src/edu/lamda/gui/GestionReservationController.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.lamda.gui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import edu.lamda.entities.Evenement;
+import edu.lamda.services.ServiceEvenement;
+import java.net.URL;
+import java.sql.Date;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Letaief Sofiene
+ */
+public class GestionReservationController implements Initializable {
+
+    @FXML
+    private AnchorPane selectPane;
+    @FXML
+    private Label errorlabel;
+   @FXML
+    private TableView<Evenement> TableV;
+   @FXML
+    private TableColumn<Evenement , String> tvNom;
+    @FXML
+    private TableColumn<Evenement , String> tvLieu;
+    @FXML
+    private TableColumn<Evenement , String> tvHeure;
+    @FXML
+    private TableColumn<Evenement , Integer> tvNbpart;
+    @FXML
+    private TableColumn<Evenement , Date> tvDate;
+    @FXML
+    private FontAwesomeIconView bnRefresh;
+    @FXML
+    private JFXTextField searchTf;
+    @FXML
+    private JFXButton reserve;
+    @FXML
+    private JFXTextField nombreTf;
+    @FXML
+    private AnchorPane toppane;
+    @FXML
+    private JFXButton minimisebtn;
+    @FXML
+    private JFXButton closebtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    
+         int idCurrentUpdatedEvent;
+     ServiceEvenement se = new ServiceEvenement();
+     ObservableList list = FXCollections.observableArrayList();
+    @FXML
+    private JFXTextField nombreTf1;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        loadDataEvent();
+         TableV.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue)
+                -> {
+                  
+                    if(newValue!=null){
+                          System.out.println(newValue);
+                          
+                          idCurrentUpdatedEvent=((Evenement) newValue).getId();
+                          
+                          
+                    // nombreTf1.setText(((Evenement) newValue).get());
+               }
+
+        });
+        
+        
+        //nombreTf1
+    }    
+
+  
+  
+
+    @FXML
+    private void refreshTable(MouseEvent event) {
+        
+           list.clear();
+           list.addAll(se.listEvenement());
+        
+          TableV.setItems(list);
+         
+    }
+
+        
+      @FXML
+    private void filter(KeyEvent event) {
+        list.clear();
+        // System.out.println("heyy yuuu");
+        list.addAll(se.listEvenement().stream().filter((evenement)
+                -> evenement.getNom().toLowerCase().contains(searchTf.getText().toLowerCase())
+                || evenement.getLieu().toLowerCase().contains(searchTf.getText().toLowerCase())
+                || String.valueOf(evenement.getNbpart()).toLowerCase().contains(searchTf.getText().toLowerCase())
+                || evenement.getHeure().toLowerCase().contains(searchTf.getText().toLowerCase())
+
+
+        ).collect(Collectors.toList()));
+    }      
+         
+    
+
+    @FXML
+    private void reserveEvent(ActionEvent event) {
+        
+        
+    }
+
+    @FXML
+    private void minimiseWindow(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleClose(ActionEvent event) {
+          Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+    
+    
+       public void loadDataEvent(){
+    //        ObservableList<Evenement> list=  loadEvenement();   
+          list.clear();
+          list.addAll(se.listEvenement());
+  
+
+         tvNom.setCellValueFactory(new PropertyValueFactory<Evenement, String>("nom"));
+          tvDate.setCellValueFactory(new PropertyValueFactory<Evenement, Date>("date"));
+        
+        
+        tvLieu.setCellValueFactory(new PropertyValueFactory<Evenement, String>("lieu") );
+        tvHeure.setCellValueFactory(new PropertyValueFactory<Evenement, String>("heure") );
+        tvNbpart.setCellValueFactory(new PropertyValueFactory<Evenement, Integer>("nbpart") );
+        
+
+         TableV.setItems(list);
+ 
+    }  
+    
+}
---
+++ src/edu/lamda/tools/CurrentSession.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.lamda.tools;
+
+import edu.lamda.entities.Client;
+
+/**
+ *
+ * @author Letaief Sofiene
+ */
+public class CurrentSession {
+    static Client client= new Client(1, "test Nom ", "test prenom", "let let ");
+}
